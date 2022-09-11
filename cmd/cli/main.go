0d0091eb2fc031ae8fed34ee6e8b3dea737823fe
Alec Jacobs <cajacobs5401@gmail.com>
2022-09-10 22:56:31 -0700

    feat: add command to sample business in location

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "yelp-roulette [options] [query]",
+	Use:   "yelp-roulette [query]",
 	Short: "Randomly select a restaurant from the Yelp API",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
@@ -18,21 +18,41 @@ var rootCmd = &cobra.Command{
 			accessToken = os.Getenv("YELP_ROULETTE_ACCESS_TOKEN")
 		}
 
-		yelp.NewClient(yelp.ClientConfig{Context: context.Background(), AccessToken: accessToken})
+		client := yelp.NewClient(yelp.ClientConfig{Context: context.Background(), AccessToken: accessToken})
+		business, err := client.RandomBusiness(yelp.SearchRequest{
+			Term:     args[0],
+			Location: location,
+			OpenNow:  openNow,
+			Price:    price,
+		})
+		if err != nil {
+			fatalError(err)
+		}
+		fmt.Printf("%s - %s\n%s\n", business.Name, business.Price, business.URL)
 	},
 }
 
 var (
 	accessToken string
+	location    string
+	openNow     bool
+	price       []string
 )
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&accessToken, "access-token", "t", "", "Yelp Developer API Access Token")
+	rootCmd.Flags().StringVarP(&location, "location", "l", "Santa Barbara, CA", "Location to base search results off of")
+	rootCmd.Flags().BoolVarP(&openNow, "open-now", "", false, "Filters results based on if business is open now")
+	rootCmd.Flags().StringArrayVarP(&price, "price", "p", []string{}, "Pricing levels to filter the search result with: 1 = $, 2 = $$, 3 = $$$, 4 = $$$$")
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		fatalError(err)
 	}
+}
+
+func fatalError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
--- pkg/yelp/client.go
+++ pkg/yelp/client.go
@@ -1,20 +1,14 @@
 package yelp
 
 import (
-	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
 
 	"github.com/shurcooL/graphql"
 	"golang.org/x/oauth2"
 )
-
-type ClientConfig struct {
-	Context     context.Context
-	AccessToken string
-}
-
-type Client struct {
-	client *graphql.Client
-}
 
 const endpoint = "https://api.yelp.com/v3/graphql"
 
@@ -26,5 +20,37 @@ func NewClient(config ClientConfig) *Client {
 	httpClient := oauth2.NewClient(config.Context, src)
 	client := graphql.NewClient(endpoint, httpClient)
 
-	return &Client{client: client}
+	return &Client{context: config.Context, client: client}
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Search returns all businesses matching the SearchRequest terms up to a provided maximum sample size
+func (c *Client) Search(params SearchRequest) ([]YelpBusiness, error) {
+	variables := map[string]interface{}{
+		"term":     graphql.String(params.Term),
+		"location": graphql.String(params.Location),
+		"limit":    graphql.Int(50),
+		"offset":   graphql.Int(0),
+		"openNow":  graphql.Boolean(params.OpenNow),
+		"price":    graphql.String(strings.Join(params.Price, ", ")),
+	}
+	query := searchQuery{}
+	if err := c.client.Query(c.context, &query, variables); err != nil {
+		return []YelpBusiness{}, fmt.Errorf("executing query: %w", err)
+	}
+
+	return query.Search.Business, nil
+}
+
+func (c *Client) RandomBusiness(params SearchRequest) (YelpBusiness, error) {
+	businesses, err := c.Search(params)
+	if err != nil {
+		return YelpBusiness{}, err
+	}
+
+	return businesses[rand.Intn(len(businesses))], nil
 }
